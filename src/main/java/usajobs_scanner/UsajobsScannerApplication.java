044074db46df889e42232656e7e0c8e85adb8ff3
max <mwlosev@gmail.com>
2025-04-01 08:54:46 +0300

    Первоначальный коммит

---
+++ src/main/java/usajobs_scanner/UsajobsScannerApplication.java
@@ -0,0 +1,13 @@
+package usajobs_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsajobsScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsajobsScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/usajobs_scanner/UsajobsScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package usajobs_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsajobsScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
