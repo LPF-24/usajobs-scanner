29c11a84c26eefce65fbbb3824e0c3043c4cb74f
max <mwlosev@gmail.com>
2025-04-01 09:58:46 +0300

    Добавлены классы WebClientConfig, JobSearchService, изменен класс UsaJobsScannerApplication

---
+++ src/main/java/usajobs_scanner/UsaJobsScannerApplication.java
@@ -0,0 +1,24 @@
+package usajobs_scanner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import usajobs_scanner.service.JobSearchService;
+
+@SpringBootApplication
+public class UsaJobsScannerApplication implements CommandLineRunner {
+	private final JobSearchService service;
+
+	public UsaJobsScannerApplication(JobSearchService service) {
+		this.service = service;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsaJobsScannerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		service.searchJobs("java");
+	}
+}
---
+++ src/main/java/usajobs_scanner/service/JobSearchService.java
@@ -0,0 +1,44 @@
+package usajobs_scanner.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+public class JobSearchService {
+    private final WebClient.Builder webClientBuilder;
+
+    @Value("${usajobs.user-agent}")
+    private String userAgent;
+
+    @Value("${usajobs.api-key}")
+    private String apiKey;
+
+    public void searchJobs(String keyword) {
+        WebClient client = webClientBuilder.baseUrl("https://data.usajobs.gov/api/search").build();
+
+        String url = "?Keyword=" + keyword;
+
+        client.get()
+                .uri(url)
+                .header("User-Agent", userAgent)
+                .header("Authorization-Key", apiKey)
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .doOnError(Throwable::printStackTrace)
+                .subscribe(json -> {
+                    JsonNode jobs = json.path("SearchResult").path("SearchResultItems");
+                    for (JsonNode job : jobs) {
+                        JsonNode fields = job.path("MatchedObjectDescriptor");
+                        System.out.println("Title: " + fields.path("PositionTitle").asText());
+                        System.out.println("Agency: " + fields.path("OrganizationName").asText());
+                        System.out.println("Location: " + fields.path("PositionLocationDisplay").asText());
+                        System.out.println("URL: " + fields.path("PositionURI").asText());
+                        System.out.println("-".repeat(50));
+                    }
+                });
+    }
+}
